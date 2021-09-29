122e74bd9cfce03381babf94f84e96b34b04bcaa
basava888 <basava.javadev@gmail.com>
2021-09-29 18:40:27 +0530

    UserService Comments added

--- src/main/java/net/javaguides/springboot/springsecurity/service/UserService.java
+++ src/main/java/net/javaguides/springboot/springsecurity/service/UserService.java
@@ -6,7 +6,9 @@ import net.javaguides.springboot.springsecurity.model.User;
 import net.javaguides.springboot.springsecurity.web.dto.UserRegistrationDto;
 
 public interface UserService extends UserDetailsService {
-
+/**
+ * Comment for finding Email By findByEmail
+ */
     User findByEmail(String email);
 
     User save(UserRegistrationDto registration);
